d3fe65dfe22dd9078729984499aed80a294f6636
Mujahid Nawaz <119625876+mujahidnawaz@users.noreply.github.com>
2024-02-17 10:39:02 +0000

    New files and new code

---
+++ Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accounntId = 104326
+let accountEmail = "Mujahid@gmail.com";
+let accountState;
+var accountPassword = "12345"
+accountCity = "Lahore"
+// accounntId = 2
+accountEmail = "Nawaz@123.com"
+accountPassword = "123456"
+accountCity = "Fsd"
+
+console.log(accounntId);
+console.table([accounntId, accountEmail, accountPassword, accountCity, accountState])
---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,6 @@
+"use strict"
+let Temp = null;
+let name = "";
+let num = 12;
+ 
+console.table([typeof Temp, typeof name, typeof num]);
